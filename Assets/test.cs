08b254054fb1672713137e6c15438e4cc5e9ca18
yuga <yuga.ichinohe@gmail.com>
2017-04-30 16:37:20 +0900

    add code

---
+++ Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; //追加
+
+public class Practice : MonoBehaviour {
+	public Text text; //追加
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello Github!"; //追加
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
